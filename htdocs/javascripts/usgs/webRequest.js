e56750000068a7b5b24670126ba3833c4b066867
llorzol <llorzol@gmail.com>
2026-09-14 20:01:04 -0700

    version 1.11 Apr 19 2026

--- htdocs/javascripts/usgs/webRequest.js
+++ htdocs/javascripts/usgs/webRequest.js
@@ -3,13 +3,15 @@
  *
  * WebRequest is a JavaScript library to make a Ajax request.
  *
- * version 1.09
- * August 25, 2017
+ * $Id: /var/www/html/columbia/javascripts/usgs/webRequest.js, v 1.11 2026/04/19 16:24:27 llorzol Exp $
+ * $Revision: 1.11 $
+ * $Date: 2026/04/19 16:24:27 $
+ * $Author: llorzol $
 */
 
 /*
 ###############################################################################
-# Copyright (c) 2017 Oregon Water Science Center
+# Copyright (c) Oregon Water Science Center
 # 
 # Permission is hereby granted, free of charge, to any person obtaining a
 # copy of this software and associated documentation files (the "Software"),
@@ -30,6 +32,50 @@
 # DEALINGS IN THE SOFTWARE.
 ###############################################################################
 */
+
+async function webRequests(urls, responseType, callBack) {
+    // Create an array of Promises
+    //
+    const fetchPromises = urls.map(url => fetch(url));
+
+    try {
+        // Wait for all promises to resolve
+        //
+        const responses = await Promise.all(fetchPromises);
+
+        // Map over the responses to parse them as JSON (each is an async operation)
+        //
+        const myPromises = responses.map(response => {
+            if (!response.ok) {
+                // You must handle non-ok statuses here
+                throw new Error(`HTTP error! status: ${response.status} for ${response.url}`);
+            }
+            if (responseType == 'json') return response.json();
+            else if (responseType == 'text') return response.text();
+            else if (responseType == 'image') return response.arrayBuffer();
+            else if (responseType == 'blob') return response.blob();
+            else if (responseType == 'bytes') return response.bytes();
+            else return response.text();
+        });
+
+        // Wait for all JSON parsing promises to resolve
+        //
+        const dataArray = await Promise.all(myPromises);
+        //console.log("All concurrent fetches complete:", dataArray);
+        //return dataArray;
+        callBack(dataArray)
+
+    } catch (error) {
+        let message = `An error occurred during concurrent fetches: ${error}`
+        console.error(message)
+        updateModal(message);
+        fadeModal(6000);
+    }
+}
+
+
+
+
 
 function webRequest(request_type, script_http, data_http, dataType, callFunction)
   {
@@ -54,6 +100,10 @@ function webRequest(request_type, script_http, data_http, dataType, callFunction
                {
                  message = "Not connect.n Verify Network.";
                }
+            else if (jqXHR.status == 400)
+               {
+                 message = "Bad request. [400] " . jqXHR.responseText;
+               }
             else if (jqXHR.status == 404)
                {
                  message = "Requested page not found. [404]";
@@ -76,12 +126,12 @@ function webRequest(request_type, script_http, data_http, dataType, callFunction
                }
             else
                {
-                 message = "Uncaught Error.n " + jqXHR.responseText;
+                 message = "Uncaught Error " + exception;
                }
 
             message    += "<br> please wait while the page is refreshed";
             openModal(message);
-            fadeModal(5000);
+            fadeModal(4000);
             return false;
            });
 
