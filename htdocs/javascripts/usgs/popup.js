e611bfc924366ba0a07471dca413c62a3073d1fd
llorzol <llorzol@gmail.com>
2026-09-14 20:01:04 -0700

    version 3.07 Sep 14 2026

--- htdocs/javascripts/usgs/popup.js
+++ htdocs/javascripts/usgs/popup.js
@@ -3,8 +3,10 @@
  *
  * Provides set of functions to build a Popup.
  *
- * version 3.03
- * July 10, 2024
+ * $Id: /var/www/html/puz/javascripts/usgs/popup.js, v 3.07 2024/09/25 15:05:11 llorzol Exp $
+ * $Revision: 3.07 $
+ * $Date: 2024/09/25 15:05:11 $
+ * $Author: llorzol $
 */
 
 /*
@@ -38,13 +40,13 @@ var popupOptions = { 'maxHeight': '200', 'maxWidth': '300', offset: new L.Point(
 // Adds a new DIV table row with two columns
 //
 function addTableRow(col1, col2) {
-   var content = '<div class="divTableRow">';
-   content    += ' <div class="divTableCell">';
+   var content = '<div class="divTableRow table-hover border-dark border-bottom border-1">';
+   content    += ' <div class="divTableCell text-start">';
    content    += col1;
    content    += ' </div>';
    if(col2.toString().length > 0)
      {
-      content    += ' <div class="divTableCell">';
+      content    += ' <div class="divTableCell text-end text-nowrap">';
       content    += col2;
       content    += ' </div>';
      }
@@ -95,10 +97,9 @@ function createPopUp(site, siteInfo)
 
    //  Create popup content
    //
-   //var popupContent  = '<div class="leaflet-popup-body">';
    var popupContent  = '<div class="leaflet-popup-title">Location Information</div>';
-       popupContent += '<div class="leaflet-popup-body">';
-       popupContent += '<div class="divTable">';
+   popupContent     += '<div class="leaflet-popup-body">';
+   popupContent     += '<div class="divTable">';
    popupContent     += '<div class="divTableBody">';
 
    // Attributes
@@ -114,9 +115,9 @@ function createPopUp(site, siteInfo)
    //
    popupContent     += addTableRow('<span class="label">Site Longitude:</span>', longitude);
    popupContent     += addTableRow('<span class="label">Site Latitude:</span>', latitude);
-   popupContent     += addTableRow('<span class="label">Estimated Depth to Groundwater:</span>', water_level);
-   popupContent     += addTableRow('<span class="label">Estimated Water Elevation of Groundwater:</span>', water_level_elevation);
-   popupContent     += addTableRow('<span class="label">Land-Surface Elevation:</span>', land_surface_elevation);
+   popupContent     += addTableRow('<span class="label">Estimated Groundwater Depth (feet):</span>', water_level);
+   popupContent     += addTableRow('<span class="label">Estimated Groundwater Elevation (feet):</span>', water_level_elevation);
+   popupContent     += addTableRow('<span class="label">Land-Surface Elevation (feet):</span>', land_surface_elevation);
    popupContent     += addTableRow('<span class="label">Uncertainty:</span>', uncertainty);
 
    popupContent     += '</div></div></div>';
@@ -124,7 +125,7 @@ function createPopUp(site, siteInfo)
    // Open popup 
    //
    //var myPopup = L.responsivePopup({ offset: [2,2] }).setContent(popupContent);
-   var myPopup = L.responsivePopup({ hasTip: true, autoPan: false, offset: [2, 2]}).setContent(popupContent);
+   var myPopup = new L.responsivePopup({ hasTip: true, autoPan: false, offset: [2, 2]}).setContent(popupContent);
    //var myPopup = site.bindPopup(popupContent, popupOptions).openPopup();
           
    site.bindPopup(myPopup);
@@ -134,10 +135,9 @@ function createPopUp(site, siteInfo)
            this.openPopup();
           });
     
-   //$(".leaflet-popup-content").before('<div class="leaflet-popup-title">Location Information</div>');
    //$(".leaflet-popup-close-button").before('<div class="leaflet-popup-title">Location Information</div>');
    //$(".leaflet-popup-content-wrapper").prepend('<div class="leaflet-popup-title">Location Information</div>');
-   $(".leaflet-popup").css("z-index", "650");
+   //$(".leaflet-popup-title").css("top", "-2.25rem");
     
    site.on('mouseout', function (e) {
            this.closePopup();
